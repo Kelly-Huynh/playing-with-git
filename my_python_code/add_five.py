41d59758242774f8834686b43a14fbc687b27e3b
Kelly-Huynh <96kellyhuynh@gmail.com>
2026-04-08 17:26:53 +0100

    Multiple adding

--- my_python_code/add_five.py
+++ my_python_code/add_five.py
@@ -4,5 +4,7 @@ def add_five(num):
     print(f"I have calculated {num} + 5 = {num_after_adding}")
     return num_after_adding
  
-result = add_five(23)
-print(result)
+result_1 = add_five(23)
+result_2 = add_five(result_1)
+result_3 = add_five(result_2)
+print(result_3)
