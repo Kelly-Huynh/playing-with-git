70a41b13242cd5a38c79951d6c8023e9941a9a54
Kelly-Huynh <96kellyhuynh@gmail.com>
2026-04-09 11:34:02 +0100

    pip and venv notes

---
+++ my_python_code/pip-and-venv.py
@@ -0,0 +1,22 @@
+# Pip - package manager for Python
+
+# Venv - the virtual environment module for python
+
+# We don't want to install libraries and packages GLOBALLY!
+
+import random
+
+print(random.randint(1,10))
+
+import pyjokes
+
+print(pyjokes.get_joke())
+
+variable_one = 10
+
+def function_one():
+    variable_one = 12
+    return variable_one
+
+import pandas
+
